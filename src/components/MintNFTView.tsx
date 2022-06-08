56b5da60ce1cd54702301c862a651e6ff9704f02
Malcolm Ahoy <colmahoy@gmail.com>
2022-06-08 16:25:23 -0400

    Testing Handle form data (#8)
    * Test uploading file
    
    * Testing with s3 url
    
    * Testing stuff
    
    Co-authored-by: Lan Nguyen <lnguyen@ripple.com>
    Co-authored-by: Malcolm Ahoy <mahoy@ripple.com>

--- src/components/MintNFTView.tsx
+++ src/components/MintNFTView.tsx
@@ -1,17 +1,27 @@
-import { buildMetadataFromFile, convertNFTMetadatToHex } from "../utilities/metadataConverter";
-import { mintNft } from "../XrplSandbox/scripts/mintNFTWithMetadata";
-import { ImageCapture } from "./ImageCapture/ImageCapture"
+import { uploadFile } from '../api';
+import {
+  buildMetadataFromFile,
+  convertNFTMetadatToHex,
+} from '../utilities/metadataConverter';
+import { mintNft } from '../XrplSandbox/scripts/mintNFTWithMetadata';
+import { ImageCapture } from './ImageCapture/ImageCapture';
 
 export const MintNFTView = () => {
-    const onSubmit = (file: File) => {
-        const metadata = buildMetadataFromFile(file);
-        const URI = convertNFTMetadatToHex(metadata);
-        mintNft(URI);
-    }
+  const onSubmit = async (file: File) => {
+    const url = `https://ahoym-sandbox-tinft-tok.s3.amazonaws.com/${file.name}`;
+    const metadata = buildMetadataFromFile(file, url);
+    const URI = convertNFTMetadatToHex(metadata);
 
-    return (
-        <div>
-            <ImageCapture onSubmit={onSubmit} onChange={() => {}} />
-        </div>
-    )
-}
+    const uploadResult = await uploadFile(file, URI);
+    console.log('uploadedResult', uploadResult);
+    if (uploadResult.ok) {
+      mintNft(URI);
+    }
+  };
+
+  return (
+    <div>
+      <ImageCapture onSubmit={onSubmit} onChange={() => {}} />
+    </div>
+  );
+};
