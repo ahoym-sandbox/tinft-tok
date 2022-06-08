9b4f5bbda89b57f114ae424016e35b426abddb50
Malcolm Ahoy <mahoy@ripple.com>
2022-06-08 13:40:37 -0400

    Add simple routing

--- src/components/AppBottomNavigation.tsx
+++ src/components/AppBottomNavigation.tsx
@@ -1,26 +1,44 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import StorefrontIcon from '@mui/icons-material/Storefront';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import Box from '@mui/material/Box';
+import { AppViews } from '../types';
 
-export const AppBottomNavigation = () => {
-  const [value, setValue] = React.useState(0);
+type AppBottomNavigationProps = {
+  currentView: AppViews;
+  onViewChange: (nav: AppViews) => void;
+};
 
+export const AppBottomNavigation = ({
+  currentView,
+  onViewChange,
+}: AppBottomNavigationProps) => {
   return (
-    <Box sx={{ width: "100%", marginTop: "16px" }}>
+    <Box sx={{ width: '100%', marginTop: '16px' }}>
       <BottomNavigation
         showLabels
-        value={value}
+        value={currentView}
         onChange={(event, newValue) => {
-          setValue(newValue);
+          onViewChange(newValue);
         }}
       >
-        <BottomNavigationAction label="Marketplace" icon={<StorefrontIcon />} />
-        <BottomNavigationAction label="Mint NFTs" icon={<CameraAltIcon />} />
-        <BottomNavigationAction label="My NFTs" icon={<AccountCircleIcon />} />
+        <BottomNavigationAction
+          label="Marketplace"
+          icon={<StorefrontIcon />}
+          value={AppViews.MARKETPLACE}
+        />
+        <BottomNavigationAction
+          label="Mint NFTs"
+          icon={<CameraAltIcon />}
+          value={AppViews.MINT_IT}
+        />
+        <BottomNavigationAction
+          label="My NFTs"
+          icon={<AccountCircleIcon />}
+          value={AppViews.GALLERY}
+        />
       </BottomNavigation>
     </Box>
   );
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export enum AppViews {
+  MARKETPLACE = 'MARKETPLACE',
+  GALLERY = 'GALLERY',
+  MINT_IT = 'MINT_IT',
+}
