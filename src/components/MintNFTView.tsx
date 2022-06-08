2b23dcd9cad6c2527bd0cc6436639eced75605ac
Ephraim Glick <eglick@ripple.com>
2022-06-08 15:33:39 -0700

    Tweak more

--- src/components/MintNFTView.tsx
+++ src/components/MintNFTView.tsx
@@ -28,19 +28,20 @@ export const MintNFTView: React.FC<MintNFTViewProps> = (props) => {
   const onSubmit = async (file: File) => {
     const extension = file.name.split('.')[1];
     const fileName = `${uuidv4()}.${extension}`;
+    setShowConfetti(true); // What could go wrong? :)
     const uploadResult = await uploadFile(file, fileName);
-
     console.log('uploadedResult', uploadResult);
     if (uploadResult.ok) {
       const url = `https://ahoym-sandbox-tinft-tok.s3.amazonaws.com/${fileName}`;
       const metadata = buildMetadataFromFile(file, url);
       const URI = convertNFTMetadatToHex(metadata);
-      setShowConfetti(true);
       await mintNft(URI);
       setShowConfetti(false);
-      redirectToGalleryView();
+      setTimeout(() => {
+        redirectToGalleryView();
+      }, 100);
     }
-  };
+  };;
 
   return (
     <div>
