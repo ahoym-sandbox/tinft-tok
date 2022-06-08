58bc7c5eca2d37fc504ac798735a7c5bd9e33a0b
Malcolm Ahoy <mahoy@ripple.com>
2022-06-08 16:50:42 -0400

    Use correct casing

--- src/components/ImageCapture/LandscapeIcon.tsx
+++ src/components/ImageCapture/LandscapeIcon.tsx
@@ -12,7 +12,7 @@ export const LandscapeIcon: React.FC = () => {
       width="100px"
       height="100px"
       viewBox="0 0 512 512"
-      enable-background="new 0 0 512 512"
+      enableBackground="new 0 0 512 512"
       xmlSpace="preserve"
     >
       <g>
