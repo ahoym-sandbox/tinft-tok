81d46e138b7593e7c14f1b4abafd69d6b0a44ec3
Malcolm Ahoy <mahoy@ripple.com>
2022-06-09 10:14:34 -0400

    Secret is gonezo

--- src/app-accounts.ts
+++ src/app-accounts.ts
@@ -5,7 +5,7 @@
 export const APP_ACCOUNTS = [
   'rndE6df4r1NVdB9Xc46Zi43ESGs3haLELi', // 'ss23Ta1pCxN9MiyGaBganizdQ84RF',
   'rsh1JPtMuaYUmrGNMxzpJeDQCdAksMU49V', // 'ssXDVgBU2GNx5Bvbuckrs1uB1me1t',
-  'rHz5Lb5LxB9JAKhLMeE7y2YKuG7XzQFBxs', // 'snu8c8d11Vr7UKvJrcH5LeD6GoNy1
+  'rHz5Lb5LxB9JAKhLMeE7y2YKuG7XzQFBxs', // '???'
 ];
 
 export const ALL_ACCOUNTS = [
