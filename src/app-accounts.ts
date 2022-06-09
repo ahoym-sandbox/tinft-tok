30471be52fff017c9ee631ed0cb5587390c9204e
Malcolm Ahoy <mahoy@ripple.com>
2022-06-08 21:09:23 -0400

    Update marketplace to only show others' NFTs

---
+++ src/app-accounts.ts
@@ -0,0 +1,10 @@
+/**
+ * "Database" of the accounts that signed up in the app
+ */
+
+export const APP_ACCOUNTS = [
+  'rndE6df4r1NVdB9Xc46Zi43ESGs3haLELi', // 'ss23Ta1pCxN9MiyGaBganizdQ84RF',
+  'rsh1JPtMuaYUmrGNMxzpJeDQCdAksMU49V', // 'ssXDVgBU2GNx5Bvbuckrs1uB1me1t',
+  'rHz5Lb5LxB9JAKhLMeE7y2YKuG7XzQFBxs', // 'snu8c8d11Vr7UKvJrcH5LeD6GoNy1
+  // 'rwVK3oG2xz839DiTf94WS5xAmb6M8VCxLo', // self, is secret_1
+];
